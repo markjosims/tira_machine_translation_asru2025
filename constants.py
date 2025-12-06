13369cd51d5bb5c1edec3564aaa75e5eef7b7036
markjosims <mjsimmons@ucsd.edu>
2025-12-05 16:43:52 -0800

    update ISO codes

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import os
 
-DATASET_DIR = os.environ.get("DATASET")
-MODEL_DIR = os.environ.get("MODEL")
+DATASET_DIR = os.environ.get("DATASETS")
+MODEL_DIR = os.environ.get("MODELS")
 
 MODEL_CHECKPOINT = "facebook/mbart-large-50-many-to-many-mmt"
 OUTPUT_DIR = os.path.join(MODEL_DIR, "tira_mbart_finetuned")
@@ -11,5 +11,5 @@ VAL_DATA_PATH = os.path.join(DATASET_DIR, "tira_mt_tokenized_validation")
 CSV_FILE = "tira_sentences.csv"
 MODEL_CHECKPOINT = "facebook/mbart-large-50-many-to-many-mmt"
 
-SRC_LANG_CODE = "tic_SD"
+SRC_LANG_CODE = "sw_KE"
 TGT_LANG_CODE = "en_XX"
