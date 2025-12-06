38dd598923b39a80e8cd50ee53325b5cc0140c6f
markjosims <mjsimmons@ucsd.edu>
2025-12-06 00:59:09 -0800

    fix config for training

--- train_canary.py
+++ train_canary.py
@@ -24,7 +24,18 @@ from nemo.collections.speechlm2 import SALM, DataModule, SALMDataset
 from nemo.core.config import hydra_runner
 from nemo.utils.exp_manager import exp_manager
 from nemo.utils.trainer_utils import resolve_trainer_cfg
+from math import ceil
 
+
+def get_batches_per_epoch(cfg):
+    grad_acc_steps = cfg['trainer']['accumulate_grad_batches']
+    train_ds_cfg = cfg['data']['train_ds']
+    train_manifest_file = train_ds_cfg['input_cfg'][0]['manifest_filepath']
+    batch_size = train_ds_cfg['batch_size']
+    with open(train_manifest_file, 'r') as f:
+        num_records = sum(1 for _ in f)
+    batches_per_epoch = ceil(num_records/batch_size)
+    return batches_per_epoch
 
 @hydra_runner(config_path="conf", config_name="salm")
 def train(cfg):
@@ -32,6 +43,12 @@ def train(cfg):
     if torch.cuda.is_available():
         torch.cuda.set_device(0)
     torch.set_float32_matmul_precision("medium")
+
+    batches_per_epoch = get_batches_per_epoch(cfg)
+    max_epochs = cfg['trainer']['max_epochs']
+    cfg['trainer']['val_check_interval'] = batches_per_epoch
+    cfg['model']['scheduler']['T_max'] = batches_per_epoch * max_epochs
+    
     trainer = Trainer(**resolve_trainer_cfg(cfg.trainer))
     log_dir = exp_manager(trainer, cfg.get("exp_manager", None))
     OmegaConf.save(cfg, log_dir / "exp_config.yaml")
@@ -40,7 +57,11 @@ def train(cfg):
         model = SALM(OmegaConf.to_container(cfg.model, resolve=True))
 
     dataset = SALMDataset(tokenizer=model.tokenizer)
-    datamodule = DataModule(cfg.data, tokenizer=model.tokenizer, dataset=dataset)
+    datamodule = DataModule(
+        cfg.data,
+        tokenizer=model.tokenizer,
+        dataset=dataset,
+    )
 
     trainer.fit(model, datamodule)
 
