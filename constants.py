12c4cfc85434635f0bf40a38a6d231e8bb110137
Mark Simmons <marks21@utexas.edu>
2025-12-05 19:08:15 -1000

    add paths for NeMO

--- constants.py
+++ constants.py
@@ -17,6 +17,9 @@ VAL_DATA_PATH = os.path.join(DATASET_DIR, "tira_mt_tokenized_validation")
 ALLOPHANT_TRAIN_PATH = os.path.join(DATASET_DIR, "tira_allophant_mt_train")
 ALLOPHANT_VAL_PATH = os.path.join(DATASET_DIR, "tira_allophant_mt_validation")
 
+ASR_DS_PATH = os.path.join(DATASET_DIR, "tira_asr_translated")
+NEMO_AUDIO_DIR = os.path.join(DATASET_DIR, "nemo")
+
 CSV_FILE = "tira_sentences.csv"
 MODEL_CHECKPOINT = "facebook/mbart-large-50-many-to-many-mmt"
 
